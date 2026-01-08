e840e4c2150f97e75464e6ef9db83728a0bcc952
Sandy264 <sp2008200518@gmail.com>
2026-01-08 22:55:10 +0530

    Delete securepay directory

--- securepay/payments/workflows/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import PaymentRequest
-
-
-class PaymentCreateForm(forms.ModelForm):
-    class Meta:
-        model = PaymentRequest
-        fields = ["beneficiary_name", "beneficiary_ref", "amount", "currency", "purpose"]
